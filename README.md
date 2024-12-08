9adac26829f4d4c9ac3a1710472dd632d1c36c11
Vishnu Sanal T <t.v.s10123@gmail.com>
2024-12-08 16:03:16 +0530

    use python :feeble_smile

---
+++ README.md
@@ -0,0 +1,2 @@
+# eye-care-reminder
+A simple desktop app to remind you to take 20/20/20 breaks for your eye health
---
+++ main.py
@@ -0,0 +1,86 @@
+import sys
+
+from PyQt6.QtCore import QTimer, Qt, QPropertyAnimation, QEasingCurve
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QLabel, QVBoxLayout, QProgressBar, QDialog
+
+
+class PopupWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.animation = None
+        self.setWindowTitle("20/20/20 Eye Health Reminder")
+        self.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.FramelessWindowHint)
+        self.setFixedSize(500, 250)
+
+        self.layout = QVBoxLayout()
+
+        self.title = QLabel("It's time to take a break!")
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.title.setFont(QFont("Arial", 24, QFont.Weight.Bold))
+        self.title.setStyleSheet("color: #e91e63;")
+        
+        self.subtitle = QLabel("Focus on something 20 meters away while the timer completes.")
+        self.subtitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.subtitle.setFont(QFont("Arial", 12))
+        self.subtitle.setStyleSheet("color: #f06292;")
+
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setMaximum(20)
+        self.progress_bar.setValue(0)
+        self.progress_bar.setFormat(f"20 seconds remaining")
+        self.progress_bar.setStyleSheet(
+            """
+            QProgressBar {
+                border: 2px solid #e91e63;
+                border-radius: 5px;
+                background-color: #f8bbd0;
+            }
+            QProgressBar::chunk {
+                background-color: #e91e63;
+            }
+            QProgressBar { 
+                color: black;
+                text-align: center; 
+            }
+            """
+        )
+
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.subtitle)
+        self.layout.addWidget(self.progress_bar)
+        self.setLayout(self.layout)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_progress)
+        self.timer.start(1000)
+
+        self.progress_value = 0
+
+    def update_progress(self):
+        self.progress_value += 1
+        self.progress_bar.setValue(self.progress_value)
+        remaining_seconds = 20 - self.progress_value
+        self.progress_bar.setFormat(f"{remaining_seconds} seconds remaining")
+        if self.progress_value >= 20:
+            self.timer.stop()
+            self.close()
+
+
+def show_popup():
+    popup = PopupWindow()
+    popup.exec()
+
+
+class ReminderApp:
+    def __init__(self):
+        self.timer = QTimer()
+        self.timer.timeout.connect(show_popup)
+        self.timer.start(20 * 60 * 1000)
+        # show_popup()  # launch initially
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    reminder = ReminderApp()
+    sys.exit(app.exec())
